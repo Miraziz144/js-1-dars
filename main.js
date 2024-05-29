bbd5e489e851b5581e19d0c55d230c103b6911ef
Miraziz144 <nurullayevmiraziz4@gmail.com>
2024-05-29 12:31:50 +0500

    js-1-dars

---
+++ main.js
@@ -0,0 +1,33 @@
+// let stakan = "To'la";
+// stakan = "Yarim"
+// stakan= "bo'sh"
+
+
+// var setka = "Mahsulotlar"
+// setka = 12
+
+
+// const ism = "Miraziz"
+// console.log(setka);
+
+// alert(ism)
+
+ let ism =  prompt("Ismingizni yoizng")
+
+ alert(ism)
+
+ let familiya =  prompt("Familiyangizni kiriting")
+
+ alert(familiya)
+
+
+ let yashashjoyi =  prompt("yashash joyingizni kiriting")
+
+ alert(yashashjoyi)
+
+
+ let fan =  prompt("Yaxshi korgan faning nima ")
+
+ alert(fan)
+
+const roxmett ="roxmett"
